4f16694600ce7dc5e15a859f955172648e023326
Kurt <kurtanderson234@gmail.com>
2017-05-07 11:43:30 -0500

    Fixed Renaming, updated for py 3.6

--- rename_files.py
+++ rename_files.py
@@ -1,12 +1,19 @@
 import os
+import re
 
 def rename_files():
-
+    #Change currentDir to have the program rename all files within it
+    currentDir = r"C:\Users\Production\Desktop\example"
     #1 Get file Names
-    file_list = os.listdir("C:\Users\Production\Desktop\example")
+    file_list = os.listdir(currentDir)
+    saved_path = os.getcwd()
 
     #2 Rename Files
+    os.chdir(currentDir)
     for file_name in file_list:
-        os.rename(file_name, file_name.translate(None, "0123456789"))
+        print("Old Name - " + file_name)
+        print("New Name - " + re.sub(r'\d+', '', file_name))
+        os.rename(file_name, re.sub(r'\d+', '', file_name))
 
 rename_files()
+
